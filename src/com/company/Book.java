e759b6c25515ce83289fa4250fc7cf87b2bf6b7b
Moran Quesnel <moques@gmail.com>
2020-10-01 12:45:19 +0200

    Initial commit

---
+++ src/com/company/Book.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Book {
+    private String name;
+    private String author;
+    private String isbn;
+    private boolean available;
+
+    public Book(String name, String author, String isbn, boolean available) {
+        this.name = name;
+        this.author = author;
+        this.isbn = isbn;
+        this.available = available;
+    }
+
+    public void setAvailable() {
+        this.available = true;
+    }
+
+    public void setUnAvailable() {
+        this.available = false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return available == book.available &&
+                name.equals(book.name) &&
+                author.equals(book.author) &&
+                isbn.equals(book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, isbn, available);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", available=" + available +
+                '}';
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+}
---
+++ src/com/company/Library.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Library {
+
+    private Map<String, Book> books =  new HashMap<String, Book>();
+
+    public void addBook(String name, String author, String isbn) {
+        Book book = new Book(name, author, isbn, true);
+
+        if (books.containsKey(isbn)){
+            System.out.println("ATTENTION : le livre exite deja !!");
+            //throw new RuntimeException("le livre exite deja");
+        }
+        books.put(isbn, book);
+    }
+
+    public void borrow(String isbn) {
+        Book currentbook = books.get(isbn);
+        if (!currentbook.isAvailable()){
+            System.out.println("Le livre n'est pas dispo !");
+        } else {
+            currentbook.setUnAvailable();
+        }
+    }
+
+    public void giveBack(String isbn) {
+        Book currentbook = books.get(isbn);
+        if (currentbook.isAvailable()){
+            System.out.println("Le livre est deja present !");
+        } else {
+            currentbook.setAvailable();
+        }
+    }
+
+    public void showAll() {
+        books.forEach((k, v) -> System.out.println((k + ":" + v)));
+    }
+
+    public void showUnavailable() {
+
+        for (Map.Entry<String, Book> book : books.entrySet()) {
+            if (!book.getValue().isAvailable()) {
+                System.out.println(book);
+            }
+
+        }
+
+    }
+
+
+}
